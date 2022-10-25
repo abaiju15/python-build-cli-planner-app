42cfdd67db88f86ac1bbfc3ddfb3f884317fd42e
Ashish Baiju <ashish.baiju@ab-inbev.com>
2022-10-25 18:36:51 -0400

    added date for reminders

---
+++ src/deadlined_reminders.py
@@ -0,0 +1,26 @@
+from abc import ABCMeta, abstractmethod, ABC
+from collections.abc import Iterable
+from dateutil.parser import parse
+from datetime import datetime
+
+class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
+    @abstractmethod
+    def is_due(self):
+        pass
+
+class DeadlinedReminder(Iterable, ABC):
+    @abstractmethod
+    def is_due(self):
+        pass
+
+class DateReminder(DeadlinedReminder):
+    def __init__(self, text, date):
+        self.date = parse(date, dayfirst=True)
+        self.text = text
+
+    def is_due(self):
+        return self.date <= datetime.now()
+
+    def __iter__(self):
+        formatted_date = self.date.isoformat()
+        return iter([self.text, formatted_date])
