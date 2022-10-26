4d9b0230e430a472880a23e08b486e091ef5d3f0
Ashish Baiju <ashish.baiju@ab-inbev.com>
2022-10-25 22:12:29 -0400

    EveningReminder works now

--- src/deadlined_reminders.py
+++ src/deadlined_reminders.py
@@ -9,6 +9,19 @@ class DeadlinedMetaReminder(Iterable, metaclass=ABCMeta):
         pass
 
 class DeadlinedReminder(Iterable, ABC):
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        if cls is not DeadlinedReminder:
+            return NotImplemented
+
+        def attr_in_hierarchy(attr):
+            return any (attr in SuperClass.__dict__ for SuperClass in subclass.__mro__)
+
+        if not all(attr_in_hierarchy(attr) for attr in ('__iter__', 'is_due')):
+            return NotImplemented
+
+        return True
+
     @abstractmethod
     def is_due(self):
         pass
